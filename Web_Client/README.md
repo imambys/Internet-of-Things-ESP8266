93808470822640bdadc6c8a274037c84795e5513
imambys <32695525+imambys@users.noreply.github.com>
2019-09-17 16:37:37 +0700

    Add files via upload

---
+++ Web_Client/README.md
@@ -0,0 +1,21 @@
+# [ENGLISH] Project 12: Web Client
+ESP-8266 IoT seems to be working as a web browser to retrieve the source page from a website and then display it to the Terminal software. In this project we will trying to access url https://www.arduino.cc/asciilogo.txt
+
+<img src="/images/12_Web Client.PNG" height="400">
+
+### In this project you will need:
+* ESP8266 IoT V2.0 (1).
+
+Check your communication port and baudrate from Hterm software like a picture above. If there are no mistakes and coonection succes, it will be displayed on Terminal software.
+
+# [BAHASA INDONESIA] Proyek 12: Web Client
+ESP-8266 IoT seolah-olah akan berfungsi sebagai web browser untuk mengambil halaman sumber dari sebuah website dan kemudian menampilkannya ke perangkat lunak Terminal. Pada proyek ini kita akan mencoba mengakses alamat url https://www.arduino.cc/asciilogo.txt
+
+<img src="/images/12_Web Client.PNG" height="400">
+
+### Modul-modul yang dibutuhkan pada proyek ini:
+* ESP8266 IoT V2.0 (1).
+
+Cek port komunikasi dan baudrate yang digunakan dari software Hterm seperti pada gambar diatas. Apabila tidak terdapat kesalahan dan koneksi sukes, maka akan ditampilkan ke software Terminal.
+
+
---
+++ Web_Client/Web_Client/Web_Client.ino
@@ -0,0 +1,54 @@
+#include <ESP8266WiFi.h>
+
+// Change the SSID according to your network
+char ssid[] = "LEARN_WIFI";
+// Change the Password according to your network
+char pass[] = "learnmore";
+
+char server[] = "rss.detik.com";
+int status = WL_IDLE_STATUS; //fungsi library esp
+
+WiFiClient client;
+
+void pageRequest() {
+  if (client.connect(server, 80)) {
+    Serial.println("connected to server");
+    client.println("GET /rss.php HTTP/1.1");
+    client.println("Host: rss.detik.com");
+    client.println("Connection: close");
+    client.println();
+  }
+  else {
+    Serial.println("connection failed");
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(ssid, pass);
+  delay(1000);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi Connected");
+  Serial.println(ssid);
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  pageRequest();
+}
+
+void loop() {
+
+if (client.available()) { //ngecek tersedia atau ngg
+    char c = client.read(); //clien ngebaca server perkarakter
+    Serial.print(c);
+  }
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting from server.");
+    delay(1000);
+    client.stop();
+  }  
+}
